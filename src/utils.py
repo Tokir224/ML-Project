4a313c6c0e477d43046d8c93d3bee25a819a70a7
Tokir-Vora <tokirvora.224@gmail.com>
2023-11-08 18:30:00 +0530

    Prediction pipeline and web app in flask

--- src/utils.py
+++ src/utils.py
@@ -57,3 +57,12 @@ def load_object(file_path):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
